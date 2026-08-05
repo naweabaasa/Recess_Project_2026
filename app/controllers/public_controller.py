1ced673f824b34f99c5afec1bb118c0e2a0dc65c
naweabaasa <abaasanawe123@gmail.com>
2026-08-05 15:41:57 +0300

    Update Public Controller

--- app/controllers/public_controller.py
+++ app/controllers/public_controller.py
@@ -2,20 +2,28 @@ from flask import Blueprint, jsonify
 # Blueprint creates routes,
 # jsonify returns JSON responses.
 
-from app.models import Category, Product # Import database models.
+from app.models import Category, Brand, Product  # Import database models.
 
 
 # Create Public Blueprint.
 public_bp = Blueprint("public", __name__, url_prefix="/api/public")
 
 
-# Displays all active products.
-# Customers can view products without authentication.
+# Displays all active categories.
 @public_bp.route("/categories", methods=["GET"])
 def categories():
     return jsonify([
-        c.to_dict() 
+        c.to_dict()
         for c in Category.query.filter_by(status="active").all()
+    ]), 200
+
+
+# Displays all active brands.
+@public_bp.route("/brands", methods=["GET"])
+def brands():
+    return jsonify([
+        b.to_dict()
+        for b in Brand.query.filter_by(status="active").all()
     ]), 200
 
 
@@ -29,12 +37,11 @@ def products():
 @public_bp.route("/products/<int:product_id>", methods=["GET"])
 def product_detail(product_id):
 
-    return jsonify(   
+    return jsonify(
         Product.query.get_or_404(product_id).to_dict()
-        ), 200   # Find product by ID or return 404 if not found.
+    ), 200   # Find product by ID or return 404 if not found.
 
 
-
-# This controller manages public access routes for customers. 
-# It allows anyone to view active product categories, active products, and details of a specific product without logging in. 
+# This controller manages public access routes for customers.
+# It allows anyone to view active product categories, active products, and details of a specific product without logging in.
 # It uses the Category and Product models to retrieve information from the database.
