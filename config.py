0276c7a7e5b8c109e62184e18671673c63a717e0
naweabaasa <abaasanawe123@gmail.com>
2026-08-05 15:45:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,13 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = "change-me"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/bread_wise_db"
+    DB_USER = os.environ.get("DB_USER", "root")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", "")
+    DB_HOST = os.environ.get("DB_HOST", "localhost")
+    DB_NAME = os.environ.get("DB_NAME", "bread_wise_db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = "change-me-jwt"
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "app", "static", "uploads")
